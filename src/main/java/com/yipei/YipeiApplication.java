b9ace801f2be53d9ec58c25295bd5ea249ef2703
lhuan <lhaaa77@163.com>
2026-07-14 17:03:34 +0800

    初始化提交

---
+++ src/main/java/com/yipei/YipeiApplication.java
@@ -0,0 +1,13 @@
+package com.yipei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YipeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YipeiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yipei/YipeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yipei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YipeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
