489a1e9e9fd745f1efadbb445cd57d7b171a57e5
Zhangfeifei <3523284767@qq.com>
2026-07-16 09:28:30 +0800

    更新部分内容

--- src/main/java/com/yipei/YipeiApplication.java
+++ src/main/java/com/yipei/YipeiApplication.java
@@ -1,9 +1,12 @@
 package com.yipei;
 
+import com.yipei.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class YipeiApplication {
 
     public static void main(String[] args) {
